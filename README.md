692f8e82d7f70b9dce4eeec8cfa20acd289d663e
jgoel <jgoel@jgoels-MacBook-Pro.local>
2020-12-23 17:46:13 -0500

    Support full notebook files

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # go-remarkable2pdf
 Go library to parse and render Remarkable lines files as PDF.
+
+# Example
+
+```
+package main
+
+import (
+	"fmt"
+	"os"
+
+	rm2pdf "github.com/poundifdef/go-remarkable2pdf"
+)
+
+func main() {
+	input, _ := os.Open("4.rm")
+	output, _ := os.Create("out.pdf")
+	rm2pdf.RenderRmFile(input, output)
+	output.Close()
+}
+
+```
--- rm2pdf.go
+++ rm2pdf.go
@@ -1,8 +1,10 @@
 package rm2pdf
 
 import (
+	"archive/zip"
 	"io"
 	"io/ioutil"
+	"strings"
 
 	"github.com/jung-kurt/gofpdf"
 	"github.com/poundifdef/go-remarkable2pdf/models"
@@ -38,10 +40,47 @@ func RenderRmFile(input io.ReadCloser, output io.Writer) error {
 // RenderRmNotebook converts an entire Remarkable Notebook
 // and renders it as a multiple-page PDF.
 //
-// input is a reader which points to the raw Notebook file, which
-// is a zip file format
+// input is the name of the Notebook, in zip, format, to open.
 //
 // output is where the PDF is written.
-func RenderRmNotebook(input io.ReadCloser, output io.Writer) string {
-	return "hello"
+func RenderRmNotebook(input string, output io.Writer) error {
+	reader, err := zip.OpenReader(input)
+	defer reader.Close()
+
+	if err != nil {
+		return err
+	}
+
+	pdf := gofpdf.New("P", "in", "letter", "")
+
+	for _, file := range reader.File {
+		if strings.HasSuffix(file.Name, ".rm") {
+			fd, err := file.Open()
+			if err != nil {
+				return err
+			}
+
+			b, err := ioutil.ReadAll(fd)
+			if err != nil {
+				return err
+			}
+
+			rm := &models.Rm{}
+			err = rm.UnmarshalBinary(b)
+			if err != nil {
+				return err
+			}
+
+			err = renderLinesFile(pdf, rm)
+
+			if err != nil {
+				return err
+			}
+
+		}
+	}
+
+	pdf.Output(output)
+
+	return nil
 }
