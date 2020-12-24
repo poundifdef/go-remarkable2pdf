450200d9c1c9fd9c9d161a9bbc3763525e72a1ca
jgoel <jgoel@jgoels-MacBook-Pro.local>
2020-12-24 10:21:34 -0500

    Implement bytes interface for handling zip files

--- rm2pdf.go
+++ rm2pdf.go
@@ -2,6 +2,7 @@ package rm2pdf
 
 import (
 	"archive/zip"
+	"bytes"
 	"io"
 	"io/ioutil"
 	"strings"
@@ -37,20 +38,9 @@ func RenderRmFile(input io.ReadCloser, output io.Writer) error {
 	return nil
 }
 
-// RenderRmNotebook converts an entire Remarkable Notebook
-// and renders it as a multiple-page PDF.
-//
-// input is the name of the Notebook, in zip, format, to open.
-//
-// output is where the PDF is written.
-func RenderRmNotebook(input string, output io.Writer) error {
-	reader, err := zip.OpenReader(input)
-	defer reader.Close()
-
-	if err != nil {
-		return err
-	}
-
+// RenderRmNotebookFromBytes is identical to RenderRmNotebook, but
+// takes a *zip.Reader as input.
+func RenderRmNotebookFromZip(reader *zip.Reader, output io.Writer) error {
 	pdf := gofpdf.New("P", "in", "letter", "")
 
 	for _, file := range reader.File {
@@ -84,4 +74,34 @@ func RenderRmNotebook(input string, output io.Writer) error {
 	pdf.Output(output)
 
 	return nil
+}
+
+// RenderRmNotebookFromBytes is identical to RenderRmNotebook, but
+// takes a byte array of the zip file as input.
+func RenderRmNotebookFromBytes(input []byte, output io.Writer) error {
+	inputReader := bytes.NewReader(input)
+	reader, err := zip.NewReader(inputReader, inputReader.Size())
+
+	if err != nil {
+		return err
+	}
+
+	return RenderRmNotebookFromZip(reader, output)
+}
+
+// RenderRmNotebook converts an entire Remarkable Notebook
+// and renders it as a multiple-page PDF.
+//
+// input is the name of the Notebook, in zip, format, to open.
+//
+// output is where the PDF is written.
+func RenderRmNotebook(input string, output io.Writer) error {
+	reader, err := zip.OpenReader(input)
+	defer reader.Close()
+
+	if err != nil {
+		return err
+	}
+
+	return RenderRmNotebookFromZip(&reader.Reader, output)
 }
